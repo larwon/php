1ac3bf4564afc92b10a04f5e5891a3f29423d9c3
larwon <74400003+larwon@users.noreply.github.com>
2021-02-15 20:05:38 +0900

    Add files via upload

---
+++ index0701.php
@@ -0,0 +1,11 @@
+<?
+	$hap = $fn + $sn;
+	$cha = $fn - $sn;
+	$gop = $fn * $sn;
+	$mok = $fn / $sn;
+
+	echo "ÇÕ : $hap <br>";
+	echo "Â÷ : $cha <br>";
+	echo "°ö : $gop <br>";
+	echo "¸ò : $mok <br>";
+?>
