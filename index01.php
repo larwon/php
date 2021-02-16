8b9d6ef6806152c75c2601a7f17036cdd428565f
larwon <74400003+larwon@users.noreply.github.com>
2021-02-16 21:49:49 +0900

    Add files via upload

---
+++ index01.php
@@ -0,0 +1,10 @@
+<?
+	$tot = $kor + $eng + $math;
+	$avg = $tot / 3;
+
+	echo "국어 : $kor <br>";
+	echo "영어 : $eng <br>";
+	echo "수학 : $math <br>";
+	echo "총점 : $tot <br>";
+	echo "평균 : $avg";
+?>
---
+++ index02.php
@@ -0,0 +1,8 @@
+<?
+	$tot = (($child * $num1) + ($adult * $num2));
+
+	echo "청소년 입장료 : $child 원 <br>";
+	echo "어른 입장료 : $adult 원 <br>";
+	echo "청소년 매수 : $num1 매, 어른 매수 : $num2 매 <br>";
+	echo "전체 입장료 : $tot 원";
+?>
